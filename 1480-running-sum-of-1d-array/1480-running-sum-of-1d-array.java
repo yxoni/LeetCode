bea5a82c04a75dd2c05f95dcbdad9c22de112856
Seoyeon Park <90109410+yxoni@users.noreply.github.com>
2025-01-07 18:02:33 +0900

    Time: 0 ms (100%), Space: 42.5 MB (80.35%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        StringBuilder sb = new StringBuilder();
+
+        int N = nums.length;
+        int[] result = new int[N];
+        result[0] = nums[0];
+        for (int i = 1; i < N; i++) {
+            result[i] = result[i-1] + nums[i];
+        }
+        
+        return result;
+    }
+}
